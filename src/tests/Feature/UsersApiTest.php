36fc49f128eb2a69fe62c2208eb843cf625c8745
saki-0000 <saki_27_maruko@yahoo.co.jp>
2022-06-30 21:31:47 +0900

    actingAsApiAdminが動く

---
+++ src/tests/Feature/UsersApiTest.php
@@ -0,0 +1,59 @@
+<?php
+
+namespace Tests\Feature;
+
+use App\Actions\ActivityType;
+use App\Auth\Role;
+use App\Auth\User;
+use Tests\TestCase;
+
+class UsersApiTest extends TestCase
+{
+    use TestsFeature;
+
+    protected $baseEndpoint = '/api/users';
+
+    protected $endpointMap = [
+        ['get', '/api/users'],
+        ['post', '/api/users'],
+        ['get', '/api/users/1'],
+        ['put', '/api/users/1'],
+        ['delete', '/api/users/1'],
+    ];
+
+    public function test_create_endpoint()
+    {
+        $this->actingAsApiAdmin();
+        /** @var Role $role */
+        $role = Role::query()->first();
+
+        $resp = $this->postJson($this->baseEndpoint, [
+            'name'        => 'Benny Boris',
+            'email'       => 'bboris@example.com',
+            'password'    => 'mysuperpass',
+            'language'    => 'it',
+            'roles'       => [$role->id],
+            'send_invite' => false,
+        ]);
+
+        $resp->assertStatus(200);
+        $resp->assertJson([
+            'name'             => 'Benny Boris',
+            'email'            => 'bboris@example.com',
+            'external_auth_id' => '',
+            'roles'            => [
+                [
+                    'id'           => $role->id,
+                    'display_name' => $role->display_name,
+                ],
+            ],
+        ]);
+        $this->assertDatabaseHas('users', ['email' => 'bboris@example.com']);
+
+        /** @var User $user */
+        $user = User::query()->where('email', '=', 'bboris@example.com')->first();
+        $this->assertActivityExists(ActivityType::USER_CREATE, null, $user->logDescriptor());
+        $this->assertEquals(1, $user->roles()->count());
+        $this->assertEquals('it', setting()->getUser($user, 'language'));
+    }
+}
---
+++ src/tests/SharedTestHelpers.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace Tests;
+
+use App\Auth\Role;
+use App\Auth\User;
+
+trait SharedTestHelpers
+{
+    protected $admin;
+    protected $editor;
+
+    /**
+     * Set the current user context to be an admin.
+     */
+    public function asAdmin()
+    {
+        return $this->actingAs($this->getAdmin());
+    }
+
+    /**
+     * Get the current admin user.
+     */
+    public function getAdmin(): User
+    {
+        if (is_null($this->admin)) {
+            $adminRole = Role::getSystemRole('admin');
+            dump($adminRole);
+            $this->admin = $adminRole->users->first();
+        }
+
+        return $this->admin;
+    }
+}
